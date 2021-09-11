4067d9da9ca1f243a3532fa553b19fedf62d8349
Cai <trieungochai.dev@gmail.com>
2021-09-11 22:09:13 +0700

    BMI Routing Challenge

--- calculator.js
+++ calculator.js
@@ -16,6 +16,18 @@ app.post("/", function (req, res) {
   res.send("The result of the calculation is: " + result);
 });
 
+app.get("/bmicalculator", function (req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function (req, res) {
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+  var result = weight / (height * height);
+
+  res.send("Your BMI is: " + result);
+});
+
 app.listen(3000, function () {
   console.log("Server is running on port 3000!");
 });
